28388a30b9f6a417ff0c4deca9f731d000b9e1a9
AnaliaMartinez82 <eliezer.avm@gmail.com>
2024-07-05 11:55:43 -0300

    agregue enum genero, formato electronico, clase libro electronico, libro fisico

---
+++ src/clases/FormatoElectronico.java
@@ -0,0 +1,5 @@
+package clases;
+
+public enum FormatoElectronico {
+    EBOOK,PDF,APPLEBOOK
+}
---
+++ src/clases/LibroElectronico.java
@@ -0,0 +1,9 @@
+package clases;
+
+public class LibroElectronico {
+    private FormatoElectronico formatoElectronico;
+    private Double tamanio;
+    private String dominio;
+
+
+}
